9523f1bbae5c15600c4efb4e6bad1b36da69562b
Sheldon Gonsalves <sheldongonsalves02@gmail.com>
2016-04-15 11:17:33 -0400

    added

---
+++ src/customTools/DBConnect.java
@@ -0,0 +1,59 @@
+//Sheldon Gonsalves
+package customTools;
+import javax.persistence.Persistence;
+
+import java.util.Date;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+
+
+import model.HrApplicant;
+import model.HrDrugtest;
+import model.HrInterviewtable;
+import model.HrLogin;
+import model.HrRole;
+
+public class DBConnect {
+
+	public DBConnect() {
+		
+	}
+	
+	public TypedQuery <HrApplicant> searchUser()
+	{
+		EntityManager em1=DBUtil.getEmFactory().createEntityManager();
+		EntityTransaction trans = em1.getTransaction();
+		TypedQuery<HrApplicant> query =em1.createQuery("SELECT h.applicantid ,h.applicantname FROM HrApplicant h",HrApplicant.class);
+		return query ;
+		
+	}
+	
+	public TypedQuery <HrApplicant> getUserDetails(long applicant_id)
+	{
+		EntityManager em1=DBUtil.getEmFactory().createEntityManager();
+		EntityTransaction trans = em1.getTransaction();
+		TypedQuery<HrApplicant> query =em1.createQuery("SELECT h FROM HrApplicant h where h.applicantid=:applicant_id",HrApplicant.class)
+				.setParameter("applicantid", applicant_id);
+		return query ;
+	
+	}
+	protected long getNextApplicantid()
+	{
+		
+		EntityManager em1=DBUtil.getEmFactory().createEntityManager();
+		TypedQuery<Long> query =em1.createQuery("SELECT max(h.applicantid) FROM HrApplicant h",Long.class);
+		
+		return (long) query.getSingleResult();
+	}
+	
+	public void insertNewApplicant(String name ,String address ,Date bday ,String job_history ,String job_refrernce ,String veteran)
+	{
+		EntityManager em1=DBUtil.getEmFactory().createEntityManager();
+		EntityTransaction trans = em1.getTransaction();
+		
+		
+	}
+}
