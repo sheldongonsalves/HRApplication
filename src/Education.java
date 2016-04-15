00deb146d76ec28458ac965585d4e33c26a2563c
chintan <chintan791@gmail.com>
2016-04-15 14:13:07 -0400

    Form draft-will start checking when parts are completed

--- src/Education.java
+++ src/Education.java
@@ -1,12 +1,17 @@
 
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import model.HrApplicant;
+import customTools.DBConnect;
 
 
 @WebServlet("/Education")
@@ -28,7 +33,9 @@ public class Education extends HttpServlet {
 	
 		HttpSession session= request.getSession() ;
 		
+		DBConnect d= new DBConnect();
 		
+	
 		
 		
 	}
