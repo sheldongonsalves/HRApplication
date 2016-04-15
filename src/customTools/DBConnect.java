2262888d6a9fd48d688ae6a259950fa24d2d531a
Sheldon Gonsalves <sheldongonsalves02@gmail.com>
2016-04-15 16:01:04 -0400

    added

--- src/customTools/DBConnect.java
+++ src/customTools/DBConnect.java
@@ -1,8 +1,9 @@
- //Sheldon Gonsalves
+//Sheldon Gonsalves
 package customTools;
 import javax.persistence.Persistence;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
@@ -40,11 +41,28 @@ public class DBConnect {
 		return query ;
 
 	}
+	protected TypedQuery <HrDrugtest> getApplicantDrugDetails(long applicant_id)
+	{
+		EntityManager em1=DBUtil.getEmFactory().createEntityManager();
+		EntityTransaction trans = em1.getTransaction();
+		TypedQuery<HrDrugtest> query =em1.createQuery("SELECT h FROM HrDrugtest h where h.hrApplicant.applicantid=:applicant_id",HrDrugtest.class)
+				.setParameter("applicantid", applicant_id);
+		return query ;
+
+	}
 	protected long getNextApplicantid()
 	{
 
 		EntityManager em1=DBUtil.getEmFactory().createEntityManager();
 		TypedQuery<Long> query =em1.createQuery("SELECT max(h.applicantid) FROM HrApplicant h",Long.class);
+
+		return (long) query.getSingleResult();
+	}
+	protected long getNextDrugTestid()
+	{
+
+		EntityManager em1=DBUtil.getEmFactory().createEntityManager();
+		TypedQuery<Long> query =em1.createQuery("SELECT max(h.drugtestid) FROM HrDrugtest h",Long.class);
 
 		return (long) query.getSingleResult();
 	}
@@ -60,6 +78,19 @@ public class DBConnect {
 		hra.setJobreference(job_refrernce);	
 		hra.setVeteranstatus(veteran);
 		insert(hra);
+	}
+	public void insertDrugTest(long applicantid  ,String alcoholtest ,String dottest,String standardpaneltest)
+	{
+		HrDrugtest hrd = new HrDrugtest();
+		HrApplicant hra =new HrApplicant();
+		hra.setApplicantid(applicantid);
+		hrd.setDrugtestid(getNextDrugTestid()+1);
+		hrd.setHrApplicant(hra);
+		hrd.setAlcoholtest(alcoholtest);
+		hrd.setDottest(dottest);
+		hrd.setStandardpaneltest(standardpaneltest);
+
+		insert(hrd);
 	}
 
 
@@ -88,6 +119,82 @@ public class DBConnect {
 
 				.setParameter("education",education)
 				.setParameter("applicantid",applicantid);
+		trans.begin();
+
+		try
+		{
+
+			query.executeUpdate();
+			trans.commit();
+
+		}
+		catch (Exception e)
+		{
+			trans.rollback();
+
+		}
+		finally
+		{
+			em1.close();
+		}
+
+	}
+
+	public void updateCitizenship(long applicantid ,String citizenship)
+	{
+		EntityManager em1 = DBUtil.getEmFactory().createEntityManager();
+		EntityTransaction trans = em1.getTransaction();
+		TypedQuery query =em1.createQuery(
+				"Update HrApplicant hr set hr.citizenstatus =:citizenship where hr.applicantid = :applicantid",HrApplicant.class)
+
+				.setParameter("citizenstatus",citizenship)
+				.setParameter("applicantid",applicantid);
+		trans.begin();
+
+		try
+		{
+
+			query.executeUpdate();
+			trans.commit();
+
+		}
+		catch (Exception e)
+		{
+			trans.rollback();
+
+		}
+		finally
+		{
+			em1.close();
+		}
+
+	}
+
+
+	public void updateDrugTest(long applicantid )
+	{
+		EntityManager em1 = DBUtil.getEmFactory().createEntityManager();
+		EntityTransaction trans = em1.getTransaction();
+		TypedQuery query=null;
+		List <HrDrugtest> drugTest =getApplicantDrugDetails(applicantid).getResultList();
+		if(drugTest.get(0).getDottest().equalsIgnoreCase("Fail")||drugTest.get(0).getAlcoholtest().equalsIgnoreCase("Fail")||drugTest.get(0).getStandardpaneltest().equalsIgnoreCase("Fail"))
+		{
+			String result="Fail";
+			query =em1.createQuery(
+					"Update HrApplicant hr set hr.drugtestresult =:result where hr.applicantid = :applicantid",HrApplicant.class)
+
+					.setParameter("drugtestresult",result)
+					.setParameter("applicantid",applicantid);
+		}
+		else
+		{
+			String result="Pass";
+			query =em1.createQuery(
+					"Update HrApplicant hr set hr.drugtestresult =:result where hr.applicantid = :applicantid",HrApplicant.class)
+
+					.setParameter("drugtestresult",result)
+					.setParameter("applicantid",applicantid);
+		}
 		trans.begin();
 
 		try
