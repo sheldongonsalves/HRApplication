b2f1c128a924e9c2358e8048cdae8dc8fe9a8080
yjchuang <yjchuang@gmail.com>
2016-04-18 16:26:22 -0400

    add Reject.java

--- src/Reject.java
+++ src/Reject.java
@@ -9,41 +9,33 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import customTools.DBConnect;
+import customTools.DBLogin;
 import model.HrInterviewtable;
 
-@WebServlet("/PreInterview")
-public class PreInterview extends HttpServlet {
+@WebServlet("/Reject")
+public class Reject extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
     
-    public PreInterview() {
+    public Reject() {
         super();
     }
 
-	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 		doPost(request, response);
 	}
 
-	
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session = request.getSession();
-		DBConnect dbc=null;
+		DBLogin dbl=null;
 		HrInterviewtable hrit=null;
 
 		long applicantId = (long)session.getAttribute("applicantid");
-		long roleId = (long)session.getAttribute("roleid");
-		if(roleId == 6 || roleId == 7) 
-		{
-		hrit = dbc.getInterviewList(applicantId).getSingleResult();
-		}
-		//if HR employee's roles are HR Manager, Hiring Manager, or Group interview
-		if (roleId == 1 || (roleId == 6 && hrit.getHrinterviewresult().equals("Pass")) || (roleId == 7 && hrit.getHminterviewresult().equals("Pass")))
-		{
-			session.setAttribute("interviewtable", hrit);
-			request.getRequestDispatcher("/Interview.jsp").forward(request, response);
-		}
+		dbl.insertNewInterviewTable(applicantId, "No", "");
+
+		session.setAttribute("interviewtable", hrit);
+		request.getRequestDispatcher("/DisplayForm.jsp").forward(request, response);
 		
 	}
 
