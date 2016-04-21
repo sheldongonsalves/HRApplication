1026b9f7b34330043133a33a6a225b80d203ebe1
yjchuang <yjchuang@gmail.com>
2016-04-21 13:37:50 -0400

    Interview.java updated

--- src/InterviewServlet.java
+++ src/InterviewServlet.java
@@ -1,6 +1,8 @@
 
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -25,7 +27,7 @@ public class InterviewServlet extends HttpServlet {
 
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+
 		doPost(request, response);
 	}
 
@@ -44,12 +46,14 @@ public class InterviewServlet extends HttpServlet {
 		//getting input from Interview page
 		//Interview status: pass/fail
 		String interviewStatus = request.getParameter("status");
-System.out.println("...................................."+interviewStatus);
+		System.out.println("...................................."+interviewStatus);
 		//Coding test: taken or not
 		String codingTest = request.getParameter("test");
 		//Coding test if taken: pass/fail
 		String codingTestStatus = request.getParameter("teststatus");
 		long applicantId = (long) session.getAttribute("applicantid");
+
+		List<HrInterviewtable> CandidateRecord =dbc.getInterviewList(applicantId).getResultList();
 
 		//assume only these 3 roles can make it here
 		if(roleid == 1) //human resource manager
@@ -64,15 +68,21 @@ System.out.println("...................................."+interviewStatus);
 			//update interview table for Hiring Manager here
 			//dbc.updateScheduleHiringManagerInterview(applicantId);
 			dbc.updateHMInterviewResult(applicantId, interviewStatus);
-			dbc.updateCodingTestTaken(applicantId, codingTest);
-			dbc.updateCodingTestResult(applicantId, codingTestStatus);
+			if(CandidateRecord.get(0).getCodingtest().equalsIgnoreCase("taken")) 
+			{
+				dbc.updateCodingTestTaken(applicantId, codingTest);
+				dbc.updateCodingTestResult(applicantId, codingTestStatus);
+			}
 		}
 		if(roleid == 7) //if(user.getHrRole().getRolename().equalsIgnoreCase("Group Interview Manager"))
 		{
 			//update interview table for group interview here
 			dbc.updateGroupInterviewResult(applicantId, interviewStatus);
-			dbc.updateCodingTestTaken(applicantId, codingTest);
-			dbc.updateCodingTestResult(applicantId, codingTestStatus);
+			if(CandidateRecord.get(0).getCodingtest().equalsIgnoreCase("taken")) 
+			{
+				dbc.updateCodingTestTaken(applicantId, codingTest);
+				dbc.updateCodingTestResult(applicantId, codingTestStatus);
+			}
 		}
 		session.setAttribute("rolename", rolename);
 		session.setAttribute("username", username);
